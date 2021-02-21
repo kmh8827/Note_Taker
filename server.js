b8feb9934dd33fdf379a61c004da111f7e834fa2
kevin <kevinhellman94@gmail.com>
2021-02-21 17:13:46 -0500

    Added reading of JSON File

--- server.js
+++ server.js
@@ -1,26 +1,35 @@
 const express = require('express');
 const path = require('path');
-const notes = require('../Note_Taker/db/db.json');
+const fs = require('fs');
 
 const app = express();
-const PORT = 3001;
+const PORT = 3000;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
 
-app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, '/public/notes.html')));
+fs.readFile('db/db.json',"utf8", (err, data) => {
+    if (err) throw err;
+    const notes = JSON.parse(data);
+    console.log(notes);
+ 
+    app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, '/public/notes.html')));
 
-app.get('/api/notes', (req, res) => res.json(notes));
+    app.get('/api/notes', (req, res) => res.sendFile(path.join(__dirname, '/db/db.json')));
 
-app.get('*', (req, res) => res.sendFile(path.join(__dirname, '/public/index.html')));
+    app.get('*', (req, res) => res.sendFile(path.join(__dirname, '/public/index.html')));
 
-app.post('/api/notes', (req, res) => {
-    const newNote = req.body;
-
-    notes.push(newNote);
-
-    res.json(newNote);
+    app.post('/api/notes', (req, res) => {
+        const newNote = req.body;
+        console.log(newNote);
+    
+        notes = notes + newNote;
+    
+        res.json(newNote);
+    });
 });
+
+
 
 app.listen(PORT, () => console.log(`App listening on PORT ${PORT}`));
