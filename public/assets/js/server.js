287b15ccd220edbb7157011ec2a95826a530f062
kevin <kevinhellman94@gmail.com>
2021-02-21 14:16:44 -0500

    added pathway for index

--- public/assets/js/server.js
+++ public/assets/js/server.js
@@ -9,4 +9,6 @@ app.use(express.json());
 
 app.get('/', (req, res) => res.sendFile(path.join(__dirname, '../../notes.html')));
 
+app.get('*', (req, res) => res.sendFile(path.join(__dirname, '../../index.html')));
+
 app.listen(PORT, () => console.log(`App listening on PORT ${PORT}`));
