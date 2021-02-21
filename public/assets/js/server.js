47043b7fc9506fd1cfcd678748f1abe3fd52a907
kevin <kevinhellman94@gmail.com>
2021-02-21 14:14:06 -0500

    added pathway for notes

--- public/assets/js/server.js
+++ public/assets/js/server.js
@@ -7,4 +7,6 @@ const PORT = 3000;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, '../../notes.html')));
+
 app.listen(PORT, () => console.log(`App listening on PORT ${PORT}`));
